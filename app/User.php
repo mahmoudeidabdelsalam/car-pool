89db68037c2d069658958b1dd8e884975e569726
Mahmoud <mahmoud@eramint.com>
2020-10-17 22:51:50 +0200

    api login user

--- app/User.php
+++ app/User.php
@@ -1,18 +1,12 @@
 <?php
-
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use SoftDeletes;
-    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -20,7 +14,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'api_token',
     ];
 
     /**
@@ -30,22 +24,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-    protected $dates = [
-        'deleted_at'
-    ];
-
-    protected $attributes = [ 
-        'menuroles' => 'user',
     ];
 }
